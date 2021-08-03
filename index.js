1daad38a67d1712b36cfc3924e5b2daa22828d9e
AkashDeep000 <akashdeepdas970@gmail.com>
2021-08-03 10:14:05 +0300

    fff

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let browser;
 (async() => {
        browser = await puppeteer.launch({
        headless: true,
-       executablePath: '/usr/bin/chromium-browser',
+       executablePath: '/usr/bin/chromium',
        args: [
        "--no-sandbox",
        "--disable-gpu",
@@ -54,7 +54,7 @@ async function Screenshot(url, text, img) {
 
    
         //variable exists, do what you want
-const html = '<head> <meta http-equiv="content-type" content="text/html; charset=utf-8"/><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet"> <title></title> </head><div class="container"> <img width="100%" src="https://source.unsplash.com/750x450/?' + img + '"/> <div class="text"> <div class="textContent">' + text + '</div></div><style type="text/css" media="all"> body{margin: auto 0; height: 400px; width: 750px;}.container{width: auto; position: relative; text-align: center; padding-top: 60%;}.text{position: absolute; top: 70%; left: 50%; width: 75%; transform: translate(-50%, -70%);}.textContent{font-size: 3rem; font-family: Cursive; background: rgb(120,141,233);background: linear-gradient(60deg, rgba(120,141,233,0.4) 0%, rgba(78,129,212,0.4) 49%, rgba(71,187,210,0.4) 100%); color: #fff; margin: 0 0; line-height: 4.1rem; padding: .5rem 1rem; backdrop-filter: blur(3px); display: inline; -webkit-box-decoration-break: clone; box-decoration-break: clone;}img{position: absolute; top: 0; left: 0; bottom: 0; right: 0;}</style>'
+const html = '<head> <meta http-equiv="content-type" content="text/html; charset=utf-8"/><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet"> <title></title> </head><div class="container"> <img width="100%" src="https://source.unsplash.com/750x450/?' + img + '"/> <div class="text"> <div class="textContent"> ' + text + ' </div></div><style type="text/css" media="all"> body{margin: auto 0; height: 400px; width: 750px;}.container{width: auto; position: relative; text-align: center; padding-top: 60%;}.text{position: absolute; top: 70%; left: 50%; width: 75%; transform: translate(-50%, -70%);}.textContent{font-size: 3rem; font-family: Cursive; background: rgb(120,141,233);background: linear-gradient(60deg, rgba(120,141,233,0.4) 0%, rgba(78,129,212,0.4) 49%, rgba(71,187,210,0.4) 100%); color: #fff; margin: 0 0; line-height: 4.1rem; padding: .5rem 1rem; backdrop-filter: blur(3px); display: inline; -webkit-box-decoration-break: clone; box-decoration-break: clone;}img{position: absolute; top: 0; left: 0; bottom: 0; right: 0;}</style>'
 
 const page = await browser.newPage();
     
